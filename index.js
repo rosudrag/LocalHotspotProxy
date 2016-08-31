1d391f025b27a13c5829bf3e604c56d21fe2bb8d
Dragos Rosu <dragos.romario@codecomputerlove.com>
2016-08-31 15:24:05 +0100

    fix: proxy rules

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var myiisproxy = new MyIISProxy("hulltrains.co.uk.local:80 to 5051");
 var myiisproxy = new MyIISProxy("v7.hillarys.local.dev:81 to 5052");
 
 var rules = {
-  '.*hillarys.*': 'http://localhost:5052', // Rule (1)
-  '.*hull.*': 'http://localhost:5051' // Rule (2)
+  'hillarys.*/': 'http://localhost:5052/', // Rule (1)
+  'hull.*/': 'http://localhost:5051/' // Rule (2)
 };
-var defaultUrl = 'http://localhost:5052';
+var defaultUrl = 'http://localhost:5052/';
 var localproxy = new LocalProxy(rules, defaultUrl, 8000);
